85388da595e31a296274fe17b0de7c2cd92f1993
abdul_wasay <abdulwasay50@gmail.com>
2019-10-29 22:18:12 +0530

    Location updated

--- app/src/main/java/com/abdul/proximitymultimedia/LocationsEnum.java
+++ app/src/main/java/com/abdul/proximitymultimedia/LocationsEnum.java
@@ -1,14 +1,10 @@
 package com.abdul.proximitymultimedia;
 
 public enum LocationsEnum {
-    // Home co-ordinates
-    //LOCATION_1("18.6183308","73.771543","location1"),
-    //LOCATION_2("19.615496","75.765218","location2");
-    // Jammu co-ordinates
-    // University of Jammu: 32.718269, 74.868504
-    // Jammu station: 32.704066, 74.876604
-    LOCATION_1("32.718269","74.868504","location1"),
-    LOCATION_2("32.704066","74.876604","location2");
+    // 18.619166, 73.771446 -> Thergaon
+    // 18.579125, 73.737141 -> Qubix
+    LOCATION_1("18.619166"," 73.771446","location1"),
+    LOCATION_2("18.579125","73.737141","location2");
 
     private final String lattitude;
     private final String longitude;
